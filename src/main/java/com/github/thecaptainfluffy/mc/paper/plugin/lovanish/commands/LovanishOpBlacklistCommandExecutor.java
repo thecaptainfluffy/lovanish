fa9cdaac30d1392e494b97888db78c4056bca9a5
Simon Westerdahl <simon.westerdahl@hotmail.se>
2025-08-04 17:20:57 +0200

    Added so it can apply to people using luckperms as well

--- src/main/java/com/github/thecaptainfluffy/mc/paper/plugin/lovanish/commands/LovanishOpBlacklistCommandExecutor.java
+++ src/main/java/com/github/thecaptainfluffy/mc/paper/plugin/lovanish/commands/LovanishOpBlacklistCommandExecutor.java
@@ -4,6 +4,8 @@ import com.github.thecaptainfluffy.mc.paper.plugin.lovanish.Lovanish;
 import com.google.gson.JsonElement;
 import net.ess3.api.IEssentials;
 import net.ess3.api.IUser;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -31,13 +33,13 @@ public class LovanishOpBlacklistCommandExecutor implements TabExecutor {
          Player player = (Player)sender;
          IUser user = ess.getUser(player);
 
-         if (player.isOp()) {
+         if (player.hasPermission("lovanish.blacklist")) {
             List<String> opsBlackList = new ArrayList<>(plugin.getLovanishResource(Lovanish.OPS_BLACKLIST)
                 .getAsJsonArray().asList().stream()
                 .map(JsonElement::getAsString)
                 .collect(Collectors.toList()));
 
-            player.sendMessage("List of blacklisted op users from vanish plugin");
+            player.sendMessage(ChatColor.GREEN + "List of blacklisted users from vanish plugin");
             for (String nickname: opsBlackList) {
                player.sendMessage(nickname);
             }
@@ -49,22 +51,6 @@ public class LovanishOpBlacklistCommandExecutor implements TabExecutor {
 
    @Override
    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-      if (sender instanceof Player) {
-         // Only provide tab-completion if the sender is a player
-         Player player = (Player) sender;
-
-         // Check player permissions or any other condition for tab-completion
-         if (player.isOp()) {
-            // Return a list of tab-completion suggestions based on args or any other criteria
-            List<String> suggestions = new ArrayList<>();
-            suggestions.add("lovanish-list");
-            // Add more suggestions based on your needs
-
-            return suggestions;
-         }
-      }
-
-      // Return an empty list to disable tab-completion for non-OP players or non-players
       return new ArrayList<>();
    }
 }
